16ab74b970f73b09bec3859f4a62fb80af75308e
Lordwill Mabalot <lordwill.mabalot@august99.com>
2018-11-11 22:40:10 +0800

    Added Dasboard and Sidebar component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navigation">
+    <md-toolbar class="md-primary">
+      <md-button class="md-icon-button" @click="showNavigation = true">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <span class="md-title">Shitpress</span>
+    </md-toolbar>
+    <md-drawer :md-active.sync="showNavigation">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title navigation-title">Shitpress</span>
+      </md-toolbar>
+
+      <md-list class="navigation-list">
+        <md-list-item class="navigation-list-item">
+          <md-icon>view_module</md-icon>
+          <router-link to="/dashboard" class="md-list-item-text">
+            Dashboard
+          </router-link>
+        </md-list-item>
+
+      </md-list>
+    </md-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  props: {},
+  data: () => ({
+    showNavigation: false
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  &-list {
+    &-item {
+      a {
+        color: inherit;
+      }
+    }
+  }
+  .md-drawer {
+    max-width: calc(100vw - 125px);
+    width: 230px;
+  }
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard">
+      <Navigation />
+      <md-app>
+        <md-app-content>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quia ipsam labore cumque perferendis, vel dolor culpa adipisci temporibus dicta molestias officia sapiente, libero corporis. Explicabo vitae enim quos earum.Officia nemo ipsa quasi, delectus dolorem nihil expedita, alias qui ut odit pariatur praesentium, aperiam deleniti? Enim, eum id praesentium provident dolore odit consequuntur accusantium amet doloremque fuga repellendus placeat!
+        </md-app-content>
+      </md-app>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  name: 'dashboard',
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+</style>
